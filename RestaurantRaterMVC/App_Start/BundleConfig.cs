365a41e82f4a97af5c9947806ebc3d8bb9040f39
Michael Milroy <mdmilroy@gmail.com>
2020-06-09 22:19:40 -0400

    added new bootstrap theme; modified all buttons to match

--- RestaurantRaterMVC/App_Start/BundleConfig.cs
+++ RestaurantRaterMVC/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace RestaurantRaterMVC
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-journal.css",
                       "~/Content/site.css"));
         }
     }
